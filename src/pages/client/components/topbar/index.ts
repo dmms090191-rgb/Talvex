7cab02a961ef713335e441f5cc17b430efe645f7
dmms090191-rgb <dmms090191@gmail.com>
2026-06-10 13:45:20 +0300

    s

--- src/pages/client/components/topbar/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ClientBadgeButton } from './ClientBadgeButton';
-export { default as ClientClockButton } from './ClientClockButton';
-export { ClientDropdownPanel, ClientDropdownHeader, ClientDropdownEmpty } from './ClientDropdownShell';
-export { ThemeOption, NotifRow, ClientAgendaNotifItem, PropositionNotifItem } from './ClientNotifItems';
--- src/pages/vendor/components/topbar/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as VendorBadgeButton } from './VendorBadgeButton';
-export { default as VendorClockButton } from './VendorClockButton';
-export { default as VendorProfileDropdown } from './VendorProfileDropdown';
-export { VendorDropdownPanel, VendorDropdownHeader, VendorDropdownEmpty } from './VendorDropdownShell';
-export { AdminNotifRow, ClientNotifRow, VendorAgendaNotifItem, VendorConfirmedItem } from './VendorNotifItems';
